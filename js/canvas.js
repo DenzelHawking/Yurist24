7d57f1753caa6e1971f900e55eb389eed029e7bb
DenzelHawking <denzelhawking@gmail.com>
2019-12-23 18:41:57 +0500

    add new

---
+++ js/canvas.js
@@ -0,0 +1,13 @@
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+
+context.beginPath();
+context.fillStyle = '#384240'
+context.moveTo(0, 0);
+context.lineTo(0, 1400);
+context.lineTo(1000, 1700);
+context.lineTo(2000, 1400);
+context.lineTo(2000, 0);
+context.lineTo(1000, 300);
+context.fill();
+// context.closePath();
